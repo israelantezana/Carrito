1d9c1484337537fb52884348e571d17942abcd30
Israel Antezana R <israelantezana@gmail.com>
2010-03-15 11:24:05 -0400

    Small identation issue solved

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-  has_attached_file :photo, :styles => {:medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   
   validates_presence_of :name, :price
   validates_numericality_of :price, :greater_than_or_equal_to => 0
